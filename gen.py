afc976d9e3b92d5c0070f1af67bf0e0affc9225c
allyhac <allyhac@gmail.com>
2026-03-19 18:44:12 +0300

    upgrade

--- gen.py
+++ gen.py
@@ -13,14 +13,15 @@ def gen(st, ls, filename):
             print(i)
 
     print("[+] ✅ Done")
+    print(f'[+]Generated {total} numbers ✅')
     if filename:
         print(f"[+] 📁 Saved {total} numbers to '{filename}'")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ally numeric wordlist generator")
-    parser.add_argument("first_number", type=int)
-    parser.add_argument("last_number", type=int)
+    parser.add_argument("first_number", type=int ,help='first number where you want your wordlists to be generated. e.g: python gen.py 1 100')
+    parser.add_argument("last_number", type=int,)
     parser.add_argument("-o", "--output", help="output file")
 
     args = parser.parse_args()
